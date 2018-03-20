8a7ccb9f293830f1a5d93b1452513844105e8e6d
Hichem2h <hichem2h@gmail.com>
2018-03-21 00:59:59 +0100

    add : full authentication system

--- coretabs/urls.py
+++ coretabs/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from coretabs import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('', views.home)
 ]
