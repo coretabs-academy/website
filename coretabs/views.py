a76a808c9aa8026396aacbacace79f3e4da47492
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-03-22 02:08:42 +0200

    Add tutorial_view

--- coretabs/views.py
+++ coretabs/views.py
@@ -1,4 +1,10 @@
+from django.http import Http404
 from django.shortcuts import render
+
 
 def home(request):
     return render(request,'home.html')
+
+
+def tutorial_view(request, category=None, id=None):
+    return render(request, f'{category}/{id}.html')
