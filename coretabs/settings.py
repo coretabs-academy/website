4862847245e40dbcd31083b28bc80022a7c86ca4
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-03-26 17:33:43 +0200

    Change default language to arabic

--- coretabs/settings.py
+++ coretabs/settings.py
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
 
 LANGUAGES = [
     ('ar', _('Arabic')),
