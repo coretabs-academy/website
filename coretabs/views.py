47372f17d42d0fa56093d7c66efc1fc6294b2024
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-03-27 20:02:11 +0200

    Redirect to home page when 404 or 500 error code occur

--- coretabs/views.py
+++ coretabs/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortoccurmport render, redirect
 from django.template import TemplateDoesNotExist
 
 from account.views import SignupView
@@ -47,17 +47,9 @@ def contributors(request):
     return render(request, "contributors.html")
 
 
-'''
- result = None
+def handler404(request):
+    return redirect('home')
 
-    if projects is None and id is None:
-        result = render(request, f'{category}/{category}.html')
-    elif projects is not None and id is None:
-        result = render(request, f'{category}/{projects}/{projects}.html')
-    elif projects is not None and id is not None:
-        result = render(request, f'{category}/{projects}/{id}.html')
-    elif projects is None and id is not None:
-        result = render(request, f'{category}/{category}/{id}.html')
 
-    return result
-'''
+def handler500(request):
+    return redirect('home')
