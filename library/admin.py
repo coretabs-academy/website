7587eac07b3051bb1a8b27e49e6cd5a59bfe6540
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-04-01 03:19:12 +0200

    Add CourseLesson & TrackCourse to admin site

--- library/admin.py
+++ library/admin.py
@@ -3,5 +3,7 @@ from . import models
 
 
 admin.site.register(models.Lesson)
+admin.site.register(models.CourseLesson)
 admin.site.register(models.Course)
 admin.site.register(models.Track)
+admin.site.register(models.TrackCourse)
