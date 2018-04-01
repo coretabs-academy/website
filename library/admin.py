608d495f7f75e43a9658549b91f3a4c10a9c240b
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-04-01 02:21:11 +0200

    Register the models to the admin site

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Lesson)
+admin.site.register(models.Course)
+admin.site.register(models.Track)
