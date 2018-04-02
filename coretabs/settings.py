5edfa03391c3986f78f4054ea2b95b2b0fbd186d
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-04-03 01:29:25 +0200

    make the api accessible by any user

--- coretabs/settings.py
+++ coretabs/settings.py
@@ -159,10 +159,4 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
-    ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
 }
