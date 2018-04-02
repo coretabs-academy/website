1ab42b3f168c2e6ccd900fb06efb57d9eb34ccc2
Ahmed Alrifai <ahmed.alrifai.97@gmail.com>
2018-04-03 00:10:13 +0200

    make JSONRenderer the default render in the api

--- coretabs/settings.py
+++ coretabs/settings.py
@@ -156,6 +156,9 @@ ALLOW_UNICODE_SLUGS = True
 # REST Framework
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
