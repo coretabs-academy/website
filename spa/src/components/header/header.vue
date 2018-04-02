17c0d7819715041ece23cebfa7340b344d541112
KHALED_LAKEHAL <khaledcollo2011@gmail.com>
2018-04-03 00:46:35 +0100

    Add cario && montserrat fonts

--- spa/src/components/header/header.vue
+++ spa/src/components/header/header.vue
@@ -1,12 +1,12 @@
 <template>
 <div class="header">
    <v-toolbar v-bind:class="{ 'toolbar--fixed': fixed,'elevation-0':!fixed }">
+   <v-toolbar-side-icon class="visible-sm-and-down"></v-toolbar-side-icon>
       <v-toolbar-items class="hidden-sm-and-down">
          <v-btn round v-for="nav in navs" :key="nav.name">{{nav.name}}</v-btn>
       </v-toolbar-items>
       <v-spacer></v-spacer>
       <v-toolbar-title>{{title}}</v-toolbar-title>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
    </v-toolbar>
 </div>
 </template>
